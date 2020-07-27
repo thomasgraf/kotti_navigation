5de496faa2e811e7c0798d30e2c87808c5b3b720
Thomas Graf <mail@thgraf.de>
2020-07-28 00:16:32 +0200

    Update fanstatic.py

--- kotti_navigation/fanstatic.py
+++ kotti_navigation/fanstatic.py
@@ -9,19 +9,19 @@ from kotti.fanstatic import view_css
 
 library = Library('kotti_navigation', 'static')
 
-style = Resource(
-    library,
-    'css/style.css',
-    minified='css/style.min.css'
-)
+#style = Resource(
+#    library,
+#    'css/style.css',
+#    minified='css/style.min.css'
+#)
 
-dropdown = Resource(
-    library,
-    'css/dropdown.css',
-    depends=[view_css],
-    minified='css/dropdown.min.css'
-)
+#dropdown = Resource(
+#    library,
+#    'css/dropdown.css',
+#    depends=[view_css],
+#    minified='css/dropdown.min.css'
+#)
 
 
-kotti_navigation = Group([style, ])
-kotti_navigation_dropdown = Group([dropdown, ])
+#kotti_navigation = Group([style, ])
+#kotti_navigation_dropdown = Group([dropdown, ])
